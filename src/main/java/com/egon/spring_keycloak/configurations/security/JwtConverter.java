92c3a0f29a7df4655e8d54f5d17037a461b5a01a
everton_goncalves <everton.goncalves.89@gmail.com>
2025-03-11 17:54:26 -0300

    feat: using keycloak roles to manage access to resources

---
+++ src/main/java/com/egon/spring_keycloak/configurations/security/JwtConverter.java
@@ -0,0 +1,26 @@
+package com.egon.spring_keycloak.configurations.security;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+public class JwtConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+    @Override
+    public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
+        Objects.requireNonNull(jwt, "jwt cannot be null");
+        Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
+        Collection<String> roles = realmAccess.get("roles");
+        List<SimpleGrantedAuthority> grants = roles.stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role)).toList();
+        
+        return new JwtAuthenticationToken(jwt, grants);
+    }
+    
+}
--- src/main/java/com/egon/spring_keycloak/controllers/ProductController.java
+++ src/main/java/com/egon/spring_keycloak/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.egon.spring_keycloak.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
     
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     public String getProducts() {
         return "Products";
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public String createProduct() {
         return "Product Created";
     }
