4d319301ad1b790c1455193e76db24935977cb0a
everton_goncalves <everton.goncalves.89@gmail.com>
2025-03-12 11:46:06 -0300

    feat: response according user role

--- src/main/java/com/egon/spring_keycloak/controllers/ProductController.java
+++ src/main/java/com/egon/spring_keycloak/controllers/ProductController.java
@@ -1,10 +1,15 @@
 package com.egon.spring_keycloak.controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.egon.spring_keycloak.controllers.dtos.ProductResponseDto;
 
 @RestController
 @RequestMapping("/products")
@@ -12,13 +17,14 @@ public class ProductController {
     
     @GetMapping
     @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
-    public String getProducts() {
-        return "Products";
+    public ResponseEntity<List<ProductResponseDto>> getProducts() {
+        return ResponseEntity.ok(
+            List.of(ProductResponseDto.create()));
     }
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public String createProduct() {
-        return "Product Created";
+    public ResponseEntity<ProductResponseDto> createProduct() {
+        return ResponseEntity.ok(ProductResponseDto.create());
     }
 }
